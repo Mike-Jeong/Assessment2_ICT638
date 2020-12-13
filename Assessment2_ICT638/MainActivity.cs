6804314f5fd6b4d2470305c05d134607c068e2c0
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2020-12-14 10:56:08 +1300

    Tasks for Mark

--- Assessment2_ICT638/MainActivity.cs
+++ Assessment2_ICT638/MainActivity.cs
@@ -1,26 +1,160 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
-using Android.Support.V7.App;
 using Android.Runtime;
+using Android.Views;
 using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Assessment2_Ict638.Models;
 
-namespace Assessment2_ICT638
+
+namespace Assessment2_Ict638
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    [Activity(Label = "LoginActivity", MainLauncher = true)]
+    public class LoginActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            Button btnlogin = FindViewById<Button>(Resource.Id.btnlogin);
+            btnlogin.Click += Btnlogin_Click;
+
+            Button btnnewregister = FindViewById<Button>(Resource.Id.btnnewregister);
+            btnnewregister.Click += Btnnewregister_Click;
+
+            
         }
+
+        private void Btnnewregister_Click(object sender, EventArgs e)
+        {
+            Intent newActivity = new Intent(this, typeof(RegisterActivity));
+            StartActivity(newActivity);
+        }
+        private static EditText edUserName;
+        private static EditText edPassword;
+        public static void clearInput()       
+        {
+            //clear input
+            edUserName.Text = "";
+            edPassword.Text = "";
+            
+        }
+
+        private void Btnlogin_Click(object sender, EventArgs e)
+        {
+
+            edUserName = FindViewById<EditText>(Resource.Id.et_id);
+            edPassword = FindViewById<EditText>(Resource.Id.et_password);
+
+            string uname = edUserName.Text.Trim();
+            string pass = edPassword.Text.Trim();
+            string name;
+            int id;
+            
+            if (checkLogin(uname, pass, out id, out name, out string username, out string password, out string phonenumber, out string country, out string email))
+            {
+                Intent homeActivity = new Intent(this, typeof(HomeActivity));
+                Bundle bundle = new Bundle();
+                bundle.PutString("name", name);
+                bundle.PutInt("id", id);
+                bundle.PutString("username", username);
+                bundle.PutString("password", password);
+                bundle.PutString("phonenumber", phonenumber);
+                bundle.PutString("country", country);
+                bundle.PutString("email", email);
+                homeActivity.PutExtra("data", bundle);
+                clearInput();
+                StartActivity(homeActivity);
+
+                
+            }
+            else
+            {
+                Toast.MakeText(this, "Invalid username or password", ToastLength.Long).Show();
+            }
+            
+
+
+            /*  Intent homeActivity = new Intent(this, typeof(HomeActivity));
+            StartActivity(homeActivity);*/
+
+        }
+
+        private bool checkLogin(string uname, string pass, out int id, out string name, out string username, out string password, out string phonenumber, out string country, out string email)
+        {
+            bool staus = false;
+            string url = "https://10.0.2.2:5001/api/Users";
+            string response = APIConnect.Get(url);
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(response);
+            id = 0;
+            name = "";
+            username = "";
+            password = "";
+            phonenumber = "";
+            country = "";
+            email = "";
+    
+            foreach (User user in users)
+            {
+                if (user.username == uname && user.password == pass)
+                {
+                    staus = true;
+                    id = user.id;
+                    name = user.name;
+                    username = user.username;
+                    password = user.password;
+                    phonenumber = user.phonenumber;
+                    country = user.country;
+                    email = user.email;
+                    break;
+                }
+            }
+            
+            return staus;
+
+        }
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            //return base.OnCreateOptionsMenu(menu);
+            return true;
+        }
+
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {//menu
+            switch (item.ItemId)
+            {
+                case Resource.Id.Logout:
+                    {
+                        Intent newActivity = new Intent(this, typeof(HomeActivity));
+                        StartActivity(newActivity);
+                        return true;
+                    }
+                case Resource.Id.EditProfile:
+                    {
+                        //data or user 
+                        Intent newActivity = new Intent(this, typeof(ProfileActivity));
+                        Bundle bundle = Intent.GetBundleExtra("user");
+                        newActivity.PutExtra("user", bundle);
+
+
+                        StartActivity(newActivity);
+                        return true;
+                    }
+
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+
     }
 }
---
+++ Assessment2_ICT638/ProfileActivity.cs
@@ -0,0 +1,255 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assessment2_Ict638.Models;
+using Android.Util;
+using Android.Gms.Maps;
+using Xamarin.Essentials;
+using Android.Gms.Maps.Model;
+using System.Threading.Tasks;
+
+namespace Assessment2_Ict638
+{
+    [Activity(Label = "ProfileActivity")]
+    public class ProfileActivity : Activity, IOnMapReadyCallback
+    {
+        int userid;
+        Bundle bundle;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Create your application here
+            SetContentView(Resource.Layout.activity_userprofile);
+            bundle = Intent.GetBundleExtra("data");
+            userid = bundle.GetInt("id");
+
+            EditText et = FindViewById<EditText>(Resource.Id.Pname);
+            EditText et2 = FindViewById<EditText>(Resource.Id.PPhonenumber);
+            TextView tv1 = FindViewById<TextView>(Resource.Id.Pemail);
+            EditText et3 = FindViewById<EditText>(Resource.Id.PCountry);
+            TextView tv3 = FindViewById<TextView>(Resource.Id.Pusername);
+            TextView tv4 = FindViewById<TextView>(Resource.Id.Ppassword);
+
+            et.Hint = bundle.GetString("name");
+            tv3.Text = bundle.GetString("username");
+            tv4.Text = bundle.GetString("password");
+            et2.Hint = bundle.GetString("phonenumber");
+            et3.Hint = bundle.GetString("country");
+            tv1.Text = bundle.GetString("email");
+
+
+            // Delete the logout and current location
+            Button btnPModify = FindViewById<Button>(Resource.Id.btnPModify);
+            Button btnPSMS = FindViewById<Button>(Resource.Id.btnPSMS);
+            Button btnPShare = FindViewById<Button>(Resource.Id.btnPShare);
+
+            btnPModify.Click += BtnPModify_Click;
+            btnPShare.Click += BtnPShare_Click;
+            btnPSMS.Click += BtnPSMS_Click;
+
+
+
+            FrameLayout mapFragContainer = FindViewById<FrameLayout>(Resource.Id.PMapFrgContainer);
+
+
+            var mapFrag = MapFragment.NewInstance();
+            FragmentManager.BeginTransaction().Add(Resource.Id.PMapFrgContainer, mapFrag, "map").Commit();
+
+            mapFrag.GetMapAsync(this);
+
+        }
+
+        private async void BtnPSMS_Click(object sender, EventArgs e)
+        {
+            string text = "Hi, Please find my contact details as requested.Email: " + bundle.GetString("email") + " Phone Number: " + bundle.GetString("phonenumber");
+            string recipient = "0226329826";
+            var message = new SmsMessage(text, new[] { recipient });
+            await Sms.ComposeAsync(message);
+        }
+
+        private async void BtnPShare_Click(object sender, EventArgs e)
+        {
+            string UDetails = "";
+            UDetails = bundle.GetString("name") + bundle.GetString("phonenumber") + bundle.GetString("email");
+            await ShareText(UDetails);
+        }
+
+        public async Task ShareText(string text)
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Text = text,
+                Title = "User Detail Share"
+            });
+        }
+
+
+        private void BtnPModify_Click(object sender, EventArgs e)
+        {
+            EditText et = FindViewById<EditText>(Resource.Id.Pname);
+            EditText et2 = FindViewById<EditText>(Resource.Id.PPhonenumber);
+            TextView tv1 = FindViewById<TextView>(Resource.Id.Pemail);
+            EditText et3 = FindViewById<EditText>(Resource.Id.PCountry);
+            TextView tv3 = FindViewById<TextView>(Resource.Id.Pusername);
+            TextView tv4 = FindViewById<TextView>(Resource.Id.Ppassword);
+
+
+
+            User u = new User();
+            u.name = et.Text;
+            u.username = tv3.Text;
+            u.password = tv4.Text;
+            u.email = tv1.Text;
+            u.country = et3.Text;
+            u.phonenumber = et2.Text;
+
+
+            ModifyUser(u, userid);
+        }
+        public string getQuotedString(string str)
+        {
+            return "\"" + str + "\"";
+        }
+
+        public void ModifyUser(User user, int Userid)
+        {
+            string url = "https://10.0.2.2:5001/api/Users" + "/" + Userid;
+
+            string json =
+                "{" +
+                getQuotedString("id")+":"+Userid+","+
+                getQuotedString("name") + ":" + getQuotedString(user.name) + "," +
+                getQuotedString("username") + ":" + getQuotedString(user.username) + "," +
+                getQuotedString("password") + ":" + getQuotedString(user.password) + "," +
+                getQuotedString("phonenumber") + ":" + getQuotedString(user.phonenumber) + "," +
+                getQuotedString("country") + ":" + getQuotedString(user.country) + "," +
+                getQuotedString("email") + ":" + getQuotedString(user.email) +
+                "}";
+
+            if (APIConnect.Put(url, json))
+            {
+                Toast.MakeText(this, "User successfully Modified!", ToastLength.Long).Show();
+            }
+        }
+
+
+        GoogleMap gMap;
+
+        //May delete the logout in profile
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            //throw new NotImplementedException();
+        
+            gMap = googleMap;
+            googleMap.MapType = GoogleMap.MapTypeNormal;
+            googleMap.UiSettings.ZoomControlsEnabled = true;
+            googleMap.UiSettings.CompassEnabled = true;
+
+            getCurLocation(googleMap);
+
+            //LatLng loc = new LatLng(lasLoc);
+            //CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+            //builder.Target(loc);
+            //builder.Zoom(20);
+            //builder.Tilt(65);
+
+            //CameraPosition cPos = builder.Build();
+            //CameraUpdate cameraUpdate = CameraUpdateFactory.NewCameraPosition(cPos);
+            //googleMap.MoveCamera(cameraUpdate);
+
+            //MarkerOptions markerOptions = new MarkerOptions();
+            //markerOptions.SetPosition(loc);
+            //markerOptions.SetTitle("NZSE City Campus");
+
+            //googleMap.AddMarker(markerOptions);
+
+            
+        }
+
+        public async void getCurLocation(GoogleMap googleMap)
+        {
+            Console.WriteLine("Test - CurrentLoc");
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    Console.WriteLine($"current Loc - Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    MarkerOptions curLoc = new MarkerOptions();
+                    curLoc.SetPosition(new LatLng(location.Latitude, location.Longitude));
+                    var address = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
+                    var placemark = address?.FirstOrDefault();
+                    var geocodeAddress = "";
+                    if (placemark != null)
+                    {
+                        geocodeAddress =
+                        $"AdminArea: {placemark.AdminArea}\n" +
+                        $"CountryCode: {placemark.CountryCode}\n" +
+                        $"CountryName: {placemark.CountryName}\n" +
+                        $"FeatureName: {placemark.FeatureName}\n" +
+                        $"Locality: {placemark.Locality}\n" +
+                        $"PostalCode: {placemark.PostalCode}\n" +
+                        $"SubAdminArea: {placemark.SubAdminArea}\n" +
+                        $"SubLocality: {placemark.SubLocality}\n" +
+                        $"SubThoroughfare: {placemark.SubThoroughfare}\n" +
+                        $"Thoroughfare: {placemark.Thoroughfare}\n";
+
+                    }
+                    curLoc.SetTitle("You are here" + geocodeAddress);
+                    curLoc.SetIcon(BitmapDescriptorFactory.DefaultMarker(BitmapDescriptorFactory.HueAzure));
+
+                    googleMap.AddMarker(curLoc);
+
+                    CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+                    builder.Target(new LatLng(location.Latitude, location.Longitude));
+                    builder.Zoom(20);
+                    builder.Tilt(65);
+
+                    CameraPosition cPos = builder.Build();
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.NewCameraPosition(cPos);
+                    googleMap.MoveCamera(cameraUpdate);
+
+
+
+
+
+                }
+                else
+                {
+                    OnMapReady(googleMap);
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                // Toast.MakeText(Activity"Feature Not Supported", ToastLength.Short);
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                // Toast.MakeText(Activity, "Feature Not Enabled", ToastLength.Short);
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                // Toast.MakeText(Activity, "Needs more permission", ToastLength.Short);
+            }
+            catch (Exception ex)
+            {
+                OnMapReady(googleMap);
+            }
+        }
+    }
+}
